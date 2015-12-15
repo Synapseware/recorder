a682bf94d3b2a36ea8b5933d54baddaaa97443c5
Ramun McCallum <matthew@synapseware.com>
2015-12-14 21:03:12 -0800

    emtpy source files

---
+++ src/recorder.cpp
@@ -0,0 +1,22 @@
+#include "recorder.h"
+
+
+
+void setup(void)
+{
+
+}
+
+
+
+int main(void)
+{
+	setup();
+
+	sei();
+
+	while(1)
+	{
+
+	}
+}
---
+++ src/recorder.h
@@ -0,0 +1,15 @@
+#ifndef __RECORDER_H__
+#define __RECORDER_H__
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+#include <avr/pgmspace.h>
+#include <avr/power.h>
+#include <avr/wdt.h>
+#include <math.h>
+
+
+
+
+#endif
